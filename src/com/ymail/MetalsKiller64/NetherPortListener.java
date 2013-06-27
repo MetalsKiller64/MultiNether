909e866b77b89404369040e7f6e13c8df9afd8e0
MetalsKiller64 <MetalsKiller64@ymail.com>
2013-06-27 20:43:22 +0200

    bugfix
    variables y and z now have the right values

--- src/com/ymail/MetalsKiller64/NetherPortListener.java
+++ src/com/ymail/MetalsKiller64/NetherPortListener.java
@@ -1,5 +1,6 @@
 package com.ymail.MetalsKiller64;
 
+import java.util.logging.Level;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
@@ -23,13 +24,16 @@ public class NetherPortListener implements Listener
 		Player player = e.getPlayer();
 		player.sendMessage("PlayerPortalEvent");
 		int x = player.getLocation().getBlock().getX();
-		int y = player.getLocation().getBlock().getZ();
-		int z = player.getLocation().getBlock().getY();
+		int y = player.getLocation().getBlock().getY();
+		int z = player.getLocation().getBlock().getZ();
 		World world = player.getWorld();
 		
+		//FIXME: z koordinate ist falsch (-2)
+		z = z+2;
 		Location ploc = new Location(world, x, y, z);
 		String linkworld = multinether.getConfig().getString("NetherLinks."+world.getName());
 		player.sendMessage(linkworld);
+		multinether.getLogger().log(Level.INFO, "teleport to coords: x={0} y={1} z={2}", new Object[]{ploc.getX(), ploc.getY(), ploc.getZ()});
 		player.teleport(new Location(Bukkit.getWorld(linkworld), ploc.getX(), ploc.getY(), ploc.getZ()));
 	}
 }
